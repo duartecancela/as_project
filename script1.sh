32d97273e3173388befc0176a7606bda4d3a746b
Duarte Cancela <duartecancela@gmail.com>
2022-04-08 19:47:26 +0100

    finish script1

--- script1.sh
+++ script1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo Hello World
+echo Script 1
 
 # initialize variables
 file_named_conf="/etc/named.conf"
@@ -16,4 +16,22 @@ sed -i "s/$allow_old/$allow_new/" $file_named_conf
 # ask user for domain name
 read -p "Insert a domain name: " domain_name
 
-# test
+# add a master zone to named.conf
+master_zone="zone \"$domain_name\" IN {\n       type master;\n       file \"/var/named/$domain_name.hosts\";\n};\n "
+marker="zone \".\""
+sed -i "/^$marker/i $master_zone" $file_named_conf
+
+echo -n  "\$ttl 38400
+@	IN	SOA	dns.estig.pt. mail.as.com. (
+			1165190726 ; serial
+			10800 ; refresh
+			3600 ; retry
+			604800 ; expire
+			38400 ; minimum
+)
+	IN	NS	server.estig.pt.
+	IN	A	192.168.0.110
+www	IN	A	192.168.0.110" >> /var/named/$domain_name.hosts
+
+# restart DNS 
+systemctl restart named
