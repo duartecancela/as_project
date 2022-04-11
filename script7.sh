e3b0ebd9a23faa3e8b2228ca55f56b8d56cec93a
Duarte Cancela <duartecancela@gmail.com>
2022-04-11 16:14:20 +0100

    update script 7

--- script7.sh
+++ script7.sh
@@ -3,14 +3,20 @@
 echo "Start Script 7"
 
 
-#sk user for a directory name inside /shared folder
+# ask user for a directory name inside /shared folder
 read -p "Insert a directory name to share inside /storage: " shared_directory
 
+# create shared directory with permissions
+mkdir -p /storage/$shared_directory
+
+# write in /etc/expots 
 echo "/storage/${shared_directory} 192.168.1.0/24(rw,hide,sync)" >> /etc/exports
 
 echo "Please execute this comand in your client machine inside the same network:
 
+yum install nfs-utils
 mount -t nfs [server_ip]:/storage/${shared_directory} /[your_folder_name]
+systemctl start nfs
 "
 
 # restart NFS service
