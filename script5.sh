71a8a8cb75dd5f99bc34b4da6511a14650c89236
Duarte Cancela <duartecancela@gmail.com>
2022-04-12 16:56:07 +0100

    update script 5

--- script5.sh
+++ script5.sh
@@ -2,19 +2,28 @@
 
 echo "Start Script 5"
 
+# test reverse master zone nslookup [domain_name] [sever_ip]
+
 # ask user for IP number and save it in 4 variables
-read -p "Insert a IP number: " ip_number
+read -p "Insert a IP number for reverse master zone: " ip_number
 IFS=. read ip1 ip2 ip3 ip4 <<< "$ip_number"
 
-# ask user for domain name
-read -p "Insert the domain name: " domain_name
+# verify if reverse master zone exist
+FILE=/var/named/${ip3}.${ip2}.${ip1}.in-addr.arpa.hosts
+if test -f "$FILE"
+then
+    echo "$FILE exists."
+else
+	
+	# ask user for domain name
+	read -p "Insert the domain name for reverse master zone: " domain_name
 
-# add a reverse master zone to named.conf beafore a specific marker
-master_zone="zone \"${ip3}.${ip2}.${ip1}.in-addr.arpa\" IN {\n       type master;\n       file \"/var/named/${ip3}.${ip2}.${ip1}.in-addr.arpa.hosts\";\n};\n "
-marker="zone \".\""
-sed -i "/^$marker/i $master_zone" /etc/named.conf
+	# add a reverse master zone to named.conf beafore a specific marker
+	master_zone="zone \"${ip3}.${ip2}.${ip1}.in-addr.arpa\" IN {\n       type master;\n       file \"/var/named/${ip3}.${ip2}.${ip1}.in-addr.arpa.hosts\";\n};\n "
+	marker="zone \".\""
+	sed -i "/^$marker/i $master_zone" /etc/named.conf
 
-# write a arpa.host file
+	# write a arpa.host file
 echo "\$ttl 38400
 @	IN	SOA	dns.estig.pt. mail.$domain_name. (
 			1165192116
@@ -26,7 +35,9 @@ echo "\$ttl 38400
 110	IN      PTR	www.$domain_name.
 111	IN	PTR	ftp.$domain_name." > /var/named/${ip3}.${ip2}.${ip1}.in-addr.arpa.hosts
 
-# restart DNS 
-systemctl restart named
+	# restart DNS 
+	systemctl restart named
+
+fi
 
 echo "Finish Script 5"
