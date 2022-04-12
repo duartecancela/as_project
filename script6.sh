eb3918b5ab2382d4175d82e5badc1559db0f48dd
Duarte Cancela <duartecancela@gmail.com>
2022-04-12 15:57:06 +0100

    finish script 9

--- script6.sh
+++ script6.sh
@@ -3,5 +3,4 @@
 echo "Start Script 6"
 
 
-
 echo "Finish Script 6"
--- script9.sh
+++ script9.sh
@@ -2,4 +2,7 @@
 
 echo "Start Script 9"
 
+# Create a incremental backup from important server folders and config files
+rsync -av --link-dest /backups/server_backup {/root,/etc/exports,/etc/passwd,/etc/shadow,/etc/group} /backups/server_backup
+
 echo "Finish Script 9"
