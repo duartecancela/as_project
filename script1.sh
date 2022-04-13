85137d2b5a788d1d641ee33e6f311d5b26551a4d
Duarte Cancela <duartecancela@gmail.com>
2022-04-13 21:47:49 +0100

    update script 1

--- script1.sh
+++ script1.sh
@@ -15,6 +15,7 @@ sed -i "s/$allow_old/$allow_new/" $file_named_conf
 
 # ask user for domain name
 read -p "Insert a domain name to create a master zone with a record type A: " domain_name
+read -p "Insert a IP for the master zone record type A: " master_zone_ip
 
 # add a master zone to named.conf beafore a specific marker
 master_zone="zone \"$domain_name\" IN {\n       type master;\n       file \"/var/named/$domain_name.hosts\";\n};\n "
@@ -30,8 +31,8 @@ echo -n  "\$ttl 38400
 			38400 ; minimum
 )
 	IN	NS	serverasproject.dev.
-	IN	A	192.168.0.110
-www	IN	A	192.168.0.110" >> /var/named/$domain_name.hosts
+	IN	A	${master_zone_ip}
+www	IN	A	${master_zone_ip}" > /var/named/$domain_name.hosts
 
 # restart DNS 
 systemctl restart named
