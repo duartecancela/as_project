1452ee673512e7d08e66e143f73fea899d9d33d3
Duarte Cancela <duartecancela@gmail.com>
2022-04-13 22:14:07 +0100

    update script 4

--- script4.sh
+++ script4.sh
@@ -3,7 +3,7 @@
 echo "Start Script 4"
 
 # ask user for domain name and DNS record type
-read -p "Insert the domain name to use with the record type? " domain_name
+read -p "Insert the domain name to insert the new record type? " domain_name
 read -p "Insert the DNS record type? A or MX :  " dns_record_type
 
 if [ $dns_record_type = "MX" -o $dns_record_type = "mx" ]; then
@@ -22,14 +22,18 @@ if [ $dns_record_type = "A" -o $dns_record_type = "a" ]; then
 
         if [ $dns_record_name = "www" ]; then
 		
+        	read -p "Insert the IP for record type name www? : " www_ip
+		
 		# write new DNS record type name for www
-                echo "$dns_record_name	IN	A	192.168.0.110" >>  /var/named/$domain_name.hosts
+                echo "$dns_record_name	IN	A	${www_ip}" >>  /var/named/$domain_name.hosts
         fi
 
         if [ $dns_record_name = "ftp" ]; then
 		
+        	read -p "Insert the IP for record type name ftp? : " ftp_ip
+		
 		# write new DNS record type name for ftp
-                echo "$dns_record_name	IN	A	192.168.0.111" >> /var/named/$domain_name.hosts
+                echo "$dns_record_name	IN	A	${ftp_ip}" >> /var/named/$domain_name.hosts
         fi
 fi
 
