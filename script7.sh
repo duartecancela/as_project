3396a99b2603c758502ab2243cf52bc84752301a
Duarte Cancela <duartecancela@gmail.com>
2022-04-13 23:03:04 +0100

    update script 7

--- script7.sh
+++ script7.sh
@@ -12,15 +12,16 @@ read -p "NFS Menu options - Please choose a number? :
 
 
 if [ $menu_option = "1" ]; then
-	# ask user for a directory name inside /shared folder
+	# ask user for a directory name inside /shared folder and network ip
 	read -p "Insert a directory name to share inside /storage: " shared_directory
+	read -p "Insert the network ip: " network_ip
 
 	# create shared directory with permissions
 	mkdir -p /storage/$shared_directory
 	chmod 777 /storage -R
 
 	# write in /etc/expots 
-	echo "/storage/${shared_directory} 192.168.1.0/24(rw,hide,sync)" >> /etc/exports
+	echo "/storage/${shared_directory} ${network_ip}/24(rw,hide,sync)" >> /etc/exports
 
 	echo "Please execute this comand in your client machine inside the same network:
 
@@ -38,10 +39,11 @@ if [ $menu_option = "2" ]; then
 	# ask user which shared directory want to edit and give a new name
 	read -p "Please insert the diretory name that you want to edit? " old_directory
 	read -p "Please insert the new name for the direcory? " new_directory
-	
+	read -p "Insert the network ip: " network_ip
+
 	# edit shared diretory (delete old, write new and update directory)
 	sed -i "/\/$old_directory/ d" /etc/exports
-	echo "/storage/${new_directory} 192.168.1.0/24(rw,hide,sync)" >> /etc/exports
+	echo "/storage/${new_directory} ${network_ip}/24(rw,hide,sync)" >> /etc/exports
 	mv /storage/$old_directory /storage/$new_directory 	
 
 	# restart NFS service
