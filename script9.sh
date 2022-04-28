1d17248aa901e9512d4bea80b0f64b20ec66f4ea
Duarte Cancela <duartecancela@gmail.com>
2022-04-28 14:49:44 +0100

    update script 9

--- script9.sh
+++ script9.sh
@@ -3,6 +3,9 @@
 echo "Start Script 9"
 
 # Create a incremental backup from important server folders and config files
-rsync -av --link-dest /backups/server_backup {/root,/etc/exports,/etc/passwd,/etc/shadow,/etc/group} /backups/server_backup
+rsync -av --link-dest /backups/server_backup {/root,/etc/exports,/etc/passwd,/etc/shadow,/etc/group,/etc/named,/var/named,/etc/httpd/conf*,/var/www/,/home} /backups/server_backup
+
+# Compress backup folder with current date
+tar -zcvf /backups/server_backup/backup_`date +%d-%m-%Y`.tar.gz /backups/server_backup
 
 echo "Finish Script 9"
