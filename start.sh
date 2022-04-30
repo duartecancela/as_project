34ae52548d1efcc1e15220a38d499331e9b66b78
Duarte Cancela <duartecancela@gmail.com>
2022-04-30 14:35:45 +0100

    update start script

--- start.sh
+++ start.sh
@@ -4,15 +4,15 @@ read -p "
 
 Please select an option:
 
-[1] - Create a domain's master zone.
+[1] - Create a domain forward master zone.
 [2] - Configuration and use of the SAMBA service.
 [3] - Virtual Hosts automatically creation and configuration.
 [4] - Create type A and MX records in the DNS server automatically
 [5] - Create reverse zones.
 [6] - Delete Master Forward Zones, Virtual Hosts and Reverse Zones.
 [7] - NFS service configuration.
-[8] - Critical system files and settings backup.
-[9] - RAID 1+1 hotspare storage.
+[8] - Create critical system files and settings backup.
+[9] - Create RAID 1+1 hotspare storage.
 " menu_option
 
 if [ $menu_option = "1" ]; then
